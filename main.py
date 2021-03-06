afc6b7c70821aa0db2538dab462ada7aa9b31388
Utku Gökçen <utku.gokcen13@gmail.com>
2021-03-06 14:29:00 +0300

    Kullanıcıdan şehir isteme eklendi

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import cevir
 
 sehir = input("Şehri giriniz: ")
+sehirD = (sehir[0:1].replace("i","İ")+sehir[1:]).capitalize()
 
 api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+sehir+"&appid=24bb5a00705b2420901490c95519ab0b")
 
@@ -17,8 +18,8 @@ hava_sozlu = {
     "Rain"   : "yağmurlu"
 }
 
-print(sehir + " sıcaklığı {} K".format(result["main"]["temp"]))
-print(sehir + " sıcaklığı {} C".format(round(cevir.kelvin_to_celcius(result["main"]["temp"]),2)))
-print(sehir + " sıcaklığı {} F".format(round(cevir.celcius_to_fahrenheit(cevir.kelvin_to_celcius(result["main"]["temp"])),2)))
+print(sehirD + " sıcaklığı {} K".format(result["main"]["temp"]))
+print(sehirD + " sıcaklığı {} C".format(round(cevir.kelvin_to_celcius(result["main"]["temp"]),2)))
+print(sehirD + " sıcaklığı {} F".format(round(cevir.celcius_to_fahrenheit(cevir.kelvin_to_celcius(result["main"]["temp"])),2)))
 
-print(sehir + " havası",hava_sozlu[result["weather"][0]["main"]])
+print(sehirD + " havası",hava_sozlu[result["weather"][0]["main"]])
