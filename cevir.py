6227bd01a9f04b6314289576d92801b6c9fdb750
Utku Gökçen <utku.gokcen13@gmail.com>
2021-03-06 13:55:04 +0300

    cevir.py eklendi

---
+++ cevir.py
@@ -0,0 +1,11 @@
+def kelvin_to_celcius(kelv):
+    return kelv - 273.15
+
+def celcius_to_kelvin(celc):
+    return celc + 273.15
+
+def fahrenheit_to_celcius(fahr):
+    return (fahr - 32) / 1.8
+
+def celcius_to_fahrenheit(celc):
+    return (celc * 1.8) + 32
